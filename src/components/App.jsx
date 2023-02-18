d679db42e00b33111309859df38627968d91c68a
unknown <noe.espinoza94@gmail.com>
2023-02-18 17:28:09 -0300

    se agrega ruta y crea pag de contacto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,22 @@ import Footer from './Footer/Footer';
 import ProductListContainer from './ProductListContainer/ProductListContainer';
 import Contador from './Contador/Contador';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
+     <BrowserRouter>
       <Navbar />
-      <ProductListContainer />
-      <Footer />
-      <Contador />
+      <Routes>
+      <Route path='/' element={<ProductListContainer/>}/>
+      <Route path='/categoria/:categoria' element={<ProductListContainer/>}/>
+      </Routes>       
+      <Contador />     
+      <Footer/>
 
+     </BrowserRouter>
     </div>
   );
 }
