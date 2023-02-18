d1032a13a8b1284c92daa91821934f9b4f11671d
unknown <noe.espinoza94@gmail.com>
2023-02-18 15:30:15 -0300

    Revert "se definen rutas, agregan etiquetas link y acchivo de contacto vacia"
    This reverts commit 3adb9bf34eaddfefb9db6eb85ccea8528257c40a.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import ProductListContainer from './components/ProductListContainer/ProductListContainer';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
       <Navbar />
-      <Routes>
-      <Route path='/' element={<ProductListContainer/>}/>
-      <Route path='/categoria/:categoria' element={<ProductListContainer/>}/>
-
-      </Routes>     
-       <Footer/>
-      </BrowserRouter>
-
+      <ProductListContainer />
+      <Footer />
 
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-//import Contacto from '../Contacto/Contacto';
-
 
 const Navbar = () => {
     return (
         <nav className="  navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to={"/"}>Almacen de Barrio</Link>
+    <a className="navbar-brand" href="#">Almacen de Barrio</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
@@ -16,23 +13,22 @@ const Navbar = () => {
         <li className="nav-item">     
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={"../Contacto/Contacto"}>Contacto</Link>
+          <a className="nav-link" href="#">Contacto</a>
         </li>
         <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" data-bs-toggle="dropdown" element="#" role="button" aria-haspopup="true" aria-expanded="false">Productos por Categoria</Link>
+          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos por Categoria</a>
           <div className="dropdown-menu">
-          <ul>
-            <li> <Link className="dropdown-item" to={"/categoria/7"}>Aceites</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/6"}>Arroz Legumbres</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/1"}>Bebidas</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/5"}>Churrascos Lomitos</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/3"}>Leches</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/2"}>Margarinas, Mantequillas</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/9"}>Pastas</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/"}>Verduras Congeladas</Link></li>
-           <li>  <Link className="dropdown-item" to={"/categoria/4"}>Yogurt</Link></li>
-</ul>
-     
+            <a className="dropdown-item" href="#">Aceites</a>
+            <a className="dropdown-item" href="#">Arroz Legumbres</a>
+            <a className="dropdown-item" href="#">Bebidas</a>
+            <a className="dropdown-item" href="#">Churrascos Lomitos</a>
+            <a className="dropdown-item" href="#">Leches</a>
+            <a className="dropdown-item" href="#">Margarinas, Mantequillas</a>
+            <a className="dropdown-item" href="#">Pastas</a>
+            <a className="dropdown-item" href="#">Verduras Congeladas</a>
+            <a className="dropdown-item" href="#">Yogurt</a>
+            
+            
           </div>
         </li>
       </ul>
