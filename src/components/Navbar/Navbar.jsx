1f9535c66a0c0551f9c06668b00795c094fd893a
Jonathan Gatica <jonathangaticas@hotmail.com>
2023-02-22 17:22:17 -0300

    detalles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,19 +6,16 @@ import Secciones from './Secciones/Secciones';
 const Navbar = () => {
     return (
         <nav className="  navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-  <Link className="navbar-brand" to={"/"}>Almacen de Barrio</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarColor01">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to={"/"}>Almacen de Barrio</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+        <div className="collapse navbar-collapse" id="navbarColor01">
 
       <Secciones/>
       <BotonCarrito/>      
-      <form className="d-flex">
-        <input className="form-control me-sm-2" type="search" placeholder="Search" />
-        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      
     </div>
   </div>
 </nav>
