41d463bdd0e059f2b9098c133401a30d116619e8
unknown <noe.espinoza94@gmail.com>
2023-02-23 18:04:29 -0300

    se agrego ruta de contacto y carrito, modifica archivo json para ejecutar json-server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer/Footer';
 import ProductListContainer from './ProductListContainer/ProductListContainer';
 import Contador from './Contador/Contador';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Contacto from './Contacto/Contacto';
 
 function App() {
   return (
@@ -14,10 +14,11 @@ function App() {
       <Routes>
       <Route path='/' element={<ProductListContainer/>}/>
       <Route path='/categoria/:categoria' element={<ProductListContainer/>}/>
+      <Route path='/carrito' element={<p>Carrito</p>}/>
+      <Route path='/contacto' element={<Contacto/>}/>
       </Routes>       
       <Contador />     
       <Footer/>
-
      </BrowserRouter>
     </div>
   );
