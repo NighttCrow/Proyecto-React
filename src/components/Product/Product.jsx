ceca3fd582b3441b97ebd9f59025cc8ff83f2a29
Jonathan Gatica <jonathangaticas@hotmail.com>
2023-02-23 15:41:09 -0300

    detalles

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,14 +4,14 @@ const Product = ({prod}) => {
     return (
         
         <div className="card" style={{width: '15rem', margin: '10px 20px'}}>
-        <img src={prod.img} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{prod.nombre}</h5>
-          <p className="card-text">{prod.descripcion}</p>
-          <p className="card-text">Precio: {prod.precio}</p>
-          <p className="card-text">Stock: {prod.stock}</p>
-          <a href="#" className="btn btn-primary"><Link className="nav-link" to={`/producto/${prod.id}`}>Comprar</Link></a>
-        </div>
+          <img src={prod.img} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{prod.nombre}</h5>
+            <p className="card-text">{prod.descripcion}</p>
+            <p className="card-text">Precio: {prod.precio}</p>
+            <p className="card-text">Stock: {prod.stock}</p>
+            <a href="#" className="btn btn-primary"><Link className="nav-link" to={`/producto/${prod.id}`}>Comprar</Link></a>
+          </div>
       </div>
       
         
